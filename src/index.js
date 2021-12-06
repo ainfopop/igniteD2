7b22eac8cb766f3f6c80c286d15c6bec73263ca2
Rodrigo De Oliveira <casa.rodrigo@gmail.com>
2021-12-05 22:21:49 -0300

    Resolução desafio

--- src/index.js
+++ src/index.js
@@ -11,18 +11,80 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const user = users.find( u => u.username === username);
+
+  if (!user) {
+    return response.status(404).json({error:"User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
-function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+function checksCreateTodosUserAvailability(request, response, next) { 
+  const { user } = request;
+
+  let countTodos = 0; 
+  let pro = false;
+  if (user) {
+    for(key in user.todos) {
+      countTodos++;
+    }
+    
+    pro = user.pro;
+  }
+
+  if (!pro && countTodos >= 10) {
+    return response.status(403).json({error:"Já alcançou o número de lançamentos total para o plano livre."});
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
-}
+  const { username } = request.headers;
+  const { id } = request.params;
+ 
+  if (!validate(id)) {
+    return response.status(400).json({error:"O id não é um código válido!"});
+  }
+
+  user = users.find( u => u.username === username);
+
+  if (!user) {
+    return response.status(404).json({error:"User not found!"});
+  }
+
+  todo = user.todos.find(todo =>todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({error:"Todo not found!"});
+  }
+
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
+
+ }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const { id } = request.params;
+  user = users.find( u => u.id === id);
+
+  if (!user) {  
+    return response.status(404).json({error:"User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
